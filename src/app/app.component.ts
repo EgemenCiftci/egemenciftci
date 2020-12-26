ff5259be55b63a5ae972242ede75ff1a9908b3b6
egemen <Kasm2020*>
2020-12-26 15:50:15 +0300

    First commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'egemenciftci';
+  title = 'Egemen Çiftci';
+  currentYear = new Date().getFullYear();
 }
